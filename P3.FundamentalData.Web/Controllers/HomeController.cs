a57c5236dd9d6681213e14c9527405dcc8e700e6
Fuad Shezan Chowdhury <fuadshezan@gmail.com>
2023-08-23 18:25:31 +0600

    Screener Controller and View Added

--- P3.FundamentalData.Web/Controllers/HomeController.cs
+++ P3.FundamentalData.Web/Controllers/HomeController.cs
@@ -1,32 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using P3.FundamentalData.Web.Models;
 using System.Diagnostics;
 
 namespace P3.FundamentalData.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			ViewBag.Exchange = new List<SelectListItem>
+					 {
+						new SelectListItem { Value = "Any", Text = "Any" },
+						new SelectListItem { Value = "AMEX", Text = "AMEX" },
+						new SelectListItem { Value = "NYSE", Text = "NYSE" },
+						new SelectListItem { Value = "NASDAQ", Text = "NASDAQ" }
+					 };
+			//var defaultValue = new SelectListItem { Value = "Any", Text = "Any" };
+			//ViewBag.Exchange = new SelectList(
+			//	new List<SelectListItem>
+			//	{
+			//		new SelectListItem { Value = "AMEX", Text = "AMEX" },
+			//		 new SelectListItem { Value = "NYSE", Text = "NYSE" },
+			//		 new SelectListItem { Value = "NASDAQ", Text = "NASDAQ" }
+			//	 });
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
